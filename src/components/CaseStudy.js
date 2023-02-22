fe304c83fd42e35f5d9085357d8050bf7bd57809
daminisherathiya <daminisherathiya@gmail.com>
2023-02-22 22:41:35 +0530

    Code refactoring for casestudy section

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -5,6 +5,41 @@ import Image from "next/image";
 import case_study_1 from "../../public/images/case-study/case-study-1.jpg";
 import case_study_2 from "../../public/images/case-study/case-study-2.jpg";
 import case_study_3 from "../../public/images/case-study/case-study-3.jpg";
+
+const caseStudies = {
+  healthcareSolution: {
+    subTitle: "HEALTH CARE",
+    title: "Healthcare Solution",
+    description:
+      "Healthcare group of companies with issues that substantially lacked basic insights on lead conversion. Our SF developers made standard sales dashboards, the ability to view customized options, verified exact lead sources & campaign association of online leads to scan it easily.",
+    imageSrc: case_study_1,
+  },
+  salesforceLighting: {
+    subTitle: "FINANCE",
+    title: "Salesforce Lighting",
+    description:
+      "Our Canada-based client, peer-to-peer lending & alternative investment marketplace provides an opportunity to lend money to businesses. We successfully advanced the existing underwriting process by implementing the Salesforce Lightning console.",
+    imageSrc: case_study_2,
+  },
+  realEstateSolution: {
+    subTitle: "REALESTATE",
+    title: "Real Estate Solution",
+    description:
+      "A Reputed Real Estate Service Provider in San Francisco chose Bacancy for their Salesforce project. Our SF developers provided solutions like a unique set of integrations and custom development addressed to varied requirements, maximized their CRM, and unlocked the full power of their investment from Salesforce features.",
+    imageSrc: case_study_3,
+  },
+};
+
+const CaseStudyDescription = ({ data }) => {
+  return (
+    <>
+      <p className="mb-2 font-bold text-light-blue">{data.subTitle}</p>
+      <h3 className="mb-5 text-secondary">{data.title}</h3>
+      <p className="leading-[max(23px,1.458vw)]">{data.description}</p>
+    </>
+  );
+};
+
 const CaseStudy = () => {
   return (
     <section
@@ -24,21 +59,13 @@ const CaseStudy = () => {
           </p>
         </SectionDescription>
       </div>
-      <div className="grid gap-5 text-left md:grid-cols-3">
+      <div className="grid grid-flow-dense gap-5 text-left md:grid-cols-3">
         <div className="md:col-span-2">
-          <div className="grid h-full md:grid-cols-12">
-            <div className="bg-white p-12 text-base md:col-span-8">
-              <p className="mb-2 font-bold text-light-blue">HEALTH CARE</p>
-              <h3 className="mb-5 text-secondary">Healthcare Solution</h3>
-              <p className="leading-[max(23px,1.458vw)]">
-                Healthcare group of companies with issues that substantially
-                lacked basic insights on lead conversion. Our SF developers made
-                standard sales dashboards, the ability to view customized
-                options, verified exact lead sources & campaign association of
-                online leads to scan it easily.
-              </p>
+          <div className="grid h-full sm:grid-cols-12">
+            <div className="bg-white p-12 text-base sm:col-span-8">
+              <CaseStudyDescription data={caseStudies.healthcareSolution} />
             </div>
-            <div className="md:col-span-4">
+            <div className="sm:col-span-4">
               <Image
                 src={case_study_1}
                 alt="Healthcare Solution"
@@ -47,66 +74,29 @@ const CaseStudy = () => {
             </div>
           </div>
         </div>
-        <div className="row-span-2 flex flex-col justify-between bg-white">
-          <div className="bg-white p-12 text-base">
-            <p className="mb-2 font-bold text-light-blue">REALESTATE</p>
-            <h3 className="mb-5 text-secondary">Real Estate Solution</h3>
-            <p className="leading-[max(23px,1.458vw)]">
-              A Reputed Real Estate Service Provider in San Francisco chose
-              Bacancy for their Salesforce project. Our SF developers provided
-              solutions like a unique set of integrations and custom development
-              addressed to varied requirements, maximized their CRM, and
-              unlocked the full power of their investment from Salesforce
-              features.
-            </p>
-          </div>
-          <div>
-            <Image
-              src={case_study_2}
-              alt="Real Estate Solution"
-              className="w-full"
-            />
-          </div>
-        </div>
-        <div className="hidden md:col-span-2 md:block">
-          <div className="grid h-full md:grid-cols-12">
-            <div className="md:col-span-4">
+        <div className="md:col-span-2">
+          <div className="grid h-full grid-flow-dense sm:grid-cols-12">
+            <div className="col-start-1 col-end-13 bg-white p-12 text-base sm:col-start-5 sm:col-end-13">
+              <CaseStudyDescription data={caseStudies.salesforceLighting} />
+            </div>
+            <div className="col-start-1 col-end-13 sm:col-start-1 sm:col-end-5">
               <Image
                 src={case_study_3}
                 alt="Salesforce Lighting"
                 className="h-full w-full"
               />
             </div>
-            <div className="bg-white p-12 text-base md:col-span-8">
-              <p className="mb-2 font-bold text-light-blue">FINANCE</p>
-              <h3 className="mb-5 text-secondary">Salesforce Lighting</h3>
-              <p className="leading-[max(23px,1.458vw)]">
-                Our Canada-based client, peer-to-peer lending & alternative
-                investment marketplace provides an opportunity to lend money to
-                businesses. We successfully advanced the existing underwriting
-                process by implementing the Salesforce Lightning console.
-              </p>
-            </div>
           </div>
         </div>
-        <div className="md:col-span-2 md:hidden">
-          <div className="grid h-full md:grid-cols-12">
-            <div className="bg-white p-12 text-base md:col-span-8">
-              <p className="mb-2 font-bold text-light-blue">FINANCE</p>
-              <h3 className="mb-5 font-semibold text-secondary">
-                Salesforce Lighting
-              </h3>
-              <p className="leading-[max(23px,1.458vw)]">
-                Our Canada-based client, peer-to-peer lending & alternative
-                investment marketplace provides an opportunity to lend money to
-                businesses. We successfully advanced the existing underwriting
-                process by implementing the Salesforce Lightning console.
-              </p>
+        <div className="row-span-2 flex flex-col justify-between bg-white">
+          <div className="grid h-full sm:grid-cols-12">
+            <div className="bg-white p-12 text-base sm:col-span-8 md:col-span-12">
+              <CaseStudyDescription data={caseStudies.realEstateSolution} />
             </div>
-            <div className="md:col-span-4">
+            <div className="sm:col-span-4 md:col-span-12">
               <Image
-                src={case_study_3}
-                alt="Salesforce Lighting"
+                src={case_study_2}
+                alt="Real Estate Solution"
                 className="h-full w-full"
               />
             </div>
