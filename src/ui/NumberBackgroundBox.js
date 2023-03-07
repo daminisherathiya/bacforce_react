0285cbb154938b0f5cba3eefd0a06084ccc35774
daminisherathiya <daminisherathiya@gmail.com>
2023-03-07 10:24:56 +0530

    Reduce CSS

--- src/ui/NumberBackgroundBox.js
+++ src/ui/NumberBackgroundBox.js
@@ -1,5 +1,5 @@
 const NumberBackgroundBox = ({ additionalClasses, data }) => {
-  const allClasses = `counter-reset grid text-left mt-16${
+  const allClasses = `[counter-reset:my-counter] grid text-left mt-16${
     additionalClasses ? " " + additionalClasses : ""
   }`;
 
@@ -8,7 +8,7 @@ const NumberBackgroundBox = ({ additionalClasses, data }) => {
       {data.map((item, i) => (
         <div
           key={i}
-          className="bg-number relative overflow-hidden border border-gray p-10"
+          className="[counter-increment:my-counter] before:[content:'0'counter(my-counter)] before:absolute before:text-[max(80px,7.813vw)] before:text-[#6969690d] before:bottom-0 before:right-0 before:leading-[max(50px,5.104vw)] before:font-bold relative overflow-hidden border border-gray p-10"
         >
           <h3 className="mb-6 font-semibold text-light-blue">{item.title}</h3>
           <p>{item.description}</p>
