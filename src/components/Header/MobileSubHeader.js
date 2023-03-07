9989c574d06b1efadea57174e3a462c8f5ab791b
daminisherathiya <daminisherathiya@gmail.com>
2023-03-07 16:07:08 +0530

    Reduce CSS

--- src/components/Header/MobileSubHeader.js
+++ src/components/Header/MobileSubHeader.js
@@ -5,7 +5,11 @@ const MobileSubHeader = ({ service, open }) => {
   const { links, title } = service;
   return (
     <>
-      <button className={`submenu w-full text-left ${open ? "open" : ""}`}>
+      <button
+        className={`relative w-full text-left before:absolute before:right-0 before:top-2/4 before:h-[2px] before:w-[15px] before:-translate-y-2/4 before:rotate-[270deg] before:bg-[black] before:transition-all before:duration-300 before:ease-in before:content-[""] after:absolute after:right-0 after:top-2/4 after:h-[2px] after:w-[15px] after:-translate-y-2/4 after:bg-[black] after:transition-all after:duration-300 after:ease-in after:content-[""] ${
+          open ? "before:!rotate-0" : ""
+        }`}
+      >
         {title}
       </button>
       {/* https://stackoverflow.com/questions/73833316/add-transition-to-accordion-with-react-tailwind */}
