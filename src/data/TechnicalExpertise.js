c6f16a7cf4945a26a9d3c720ffd56f2b3c8ec439
daminisherathiya <daminisherathiya@gmail.com>
2023-03-16 12:56:46 +0530

    Add opacity in separators

--- src/data/TechnicalExpertise.js
+++ src/data/TechnicalExpertise.js
@@ -2,21 +2,21 @@ export const salesforce1TechnicalExpertise = [
   {
     key: "Salesforce UI/UX Solutions",
     value:
-      "Force.com | Salesforce1 | Pardot | Heroku | Apex | Chatter | Informatica Cloud | MuleSoft | Lightning | CPQ | Jitterbit | Customer 360 | Shield | Salesforce CLI | Ant Migration Tool | Case Management | Workbench | Data loader | Clickdeploy | FSL | Vlocity | Incident Management | Tableau Analytics | Salesforce Einstein | Work.com | LWC | Aura Components | Visualforce | Automation Tools | Customization",
+      `Force.com <span class="opacity-30">|</span> Salesforce1 <span class="opacity-30">|</span> Pardot <span class="opacity-30">|</span> Heroku <span class="opacity-30">|</span> Apex <span class="opacity-30">|</span> Chatter <span class="opacity-30">|</span> Informatica Cloud <span class="opacity-30">|</span> MuleSoft <span class="opacity-30">|</span> Lightning <span class="opacity-30">|</span> CPQ <span class="opacity-30">|</span> Jitterbit <span class="opacity-30">|</span> Customer 360 <span class="opacity-30">|</span> Shield <span class="opacity-30">|</span> Salesforce CLI <span class="opacity-30">|</span> Ant Migration Tool <span class="opacity-30">|</span> Case Management <span class="opacity-30">|</span> Workbench <span class="opacity-30">|</span> Data loader <span class="opacity-30">|</span> Clickdeploy <span class="opacity-30">|</span> FSL <span class="opacity-30">|</span> Vlocity <span class="opacity-30">|</span> Incident Management <span class="opacity-30">|</span> Tableau Analytics <span class="opacity-30">|</span> Salesforce Einstein <span class="opacity-30">|</span> Work.com <span class="opacity-30">|</span> LWC <span class="opacity-30">|</span> Aura Components <span class="opacity-30">|</span> Visualforce <span class="opacity-30">|</span> Automation Tools <span class="opacity-30">|</span> Customization`,
   },
   {
     key: "Salesforce Cloud",
     value:
-      "Service Cloud | Sales Cloud | Commerce Cloud | Financial Services Cloud | Marketing Cloud | Field Service Cloud | Community Cloud | App Cloud | Health Cloud | Experience Cloud | Industry Cloud | Analytics Cloud | IoT Cloud",
+      'Service Cloud <span class="opacity-30">|</span> Sales Cloud <span class="opacity-30">|</span> Commerce Cloud <span class="opacity-30">|</span> Financial Services Cloud <span class="opacity-30">|</span> Marketing Cloud <span class="opacity-30">|</span> Field Service Cloud <span class="opacity-30">|</span> Community Cloud <span class="opacity-30">|</span> App Cloud <span class="opacity-30">|</span> Health Cloud <span class="opacity-30">|</span> Experience Cloud <span class="opacity-30">|</span> Industry Cloud <span class="opacity-30">|</span> Analytics Cloud <span class="opacity-30">|</span> IoT Cloud',
   },
   {
     key: "AppExchange Packages",
     value:
-      "Conga Composer | Conga Sign | E3Excel | DocuSign | Dataloader.io | Nintex Drawloop DocGen",
+      'Conga Composer <span class="opacity-30">|</span> Conga Sign <span class="opacity-30">|</span> E3Excel <span class="opacity-30">|</span> DocuSign <span class="opacity-30">|</span> Dataloader.io <span class="opacity-30">|</span> Nintex Drawloop DocGen',
   },
   {
     key: "Salesforce Integration",
     value:
-      "ERP Integration | Marketing Automation Integration | Accounting Integration | E-Commerce Integration | Salesforce Email Integration | Collaboration System Integration | Social Media Integration | CTI Integration | Salesforce Database Integration | ITSM Integration",
+      'ERP Integration <span class="opacity-30">|</span> Marketing Automation Integration <span class="opacity-30">|</span> Accounting Integration <span class="opacity-30">|</span> E-Commerce Integration <span class="opacity-30">|</span> Salesforce Email Integration <span class="opacity-30">|</span> Collaboration System Integration <span class="opacity-30">|</span> Social Media Integration <span class="opacity-30">|</span> CTI Integration <span class="opacity-30">|</span> Salesforce Database Integration <span class="opacity-30">|</span> ITSM Integration',
   },
 ];
